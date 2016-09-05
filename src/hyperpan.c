c8991b1ab93fe2962d39ee55826ce71b70c5232d
wcordero <wcordero@Simone>
2016-09-04 23:15:40 -0600

    1

---
+++ src/hyperpan.c
@@ -0,0 +1,17 @@
+/*
+ * 
+ * William Cordero (2016)
+ * <william.cordero@gmail.com>
+ * 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc,char**argv){
+    int i;
+    printf("Arguments:\n");
+    for(i=0;i<argc;i++)printf("%i: %s\n", i, argv[i]);
+    return 0;
+}
