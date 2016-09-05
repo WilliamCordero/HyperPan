8dc9427718b92208ca78637b6c2bb5fd3f6f8214
wcordero <wcordero@Simone>
2016-09-04 23:45:36 -0600

    2

--- src/hyperpan.c
+++ src/hyperpan.c
@@ -10,8 +10,7 @@
 #include <stdlib.h>
 
 int main(int argc,char**argv){
-    int i;
     printf("Arguments:\n");
-    for(i=0;i<argc;i++)printf("%i: %s\n", i, argv[i]);
+    for(int i=0;i<argc;i++)printf("%i: %s\n", i, argv[i]);
     return 0;
 }
