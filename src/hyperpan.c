912ce52e462d880d8953ec66488f8d4945c16415
william.cordero <william.cordero@gmail.com>
2016-09-05 02:05:22 -0600

    3

--- src/hyperpan.c
+++ src/hyperpan.c
@@ -12,7 +12,21 @@
 
 int main(int argc,char**argv){
     int i;
-    printf("Debug:\n");
+    printf("Debug:>>\n");
     for(i=0;i<argc;i++)printf("%i: %s\n", i, argv[i]);
+
+/*    
+    if(!bcm2835_init())return 1;
+    bcm2835_gpio_fsel(RPI_GPIO_P1_21,BCM2835_GPIO_FSEL_OUTP);
+    while(1){
+        bcm2835_gpio_write(RPI_GPIO_P1_21, HIGH);
+        bcm2835_delay(500);
+        bcm2835_gpio_write(RPI_GPIO_P1_21, LOW);
+        bcm2835_delay(500);
+    }
+    bcm2835_close();
+ */
+ 
+    printf("Bye\n");
     return 0;
 }
