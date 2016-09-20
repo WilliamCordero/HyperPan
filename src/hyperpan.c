5be91dea71588f4c5d3e3c209127217243e90886
wcordero <wcordero@OverLook>
2016-09-19 21:22:48 -0600

    fixing go(r,t,p)

--- src/hyperpan.c
+++ src/hyperpan.c
@@ -15,6 +15,13 @@
 #include "stepper.h"
 #include "sphere.h"
 
+#define RHO_SLEEP   22
+#define RHO_STEP    27
+#define RHO_DIR     18
+#define RHO_M0      24
+#define RHO_M1      23
+#define RHO_MODE    32
+#define RHO_STEPS   200*8
 #define THETA_SLEEP 22
 #define THETA_STEP  27
 #define THETA_DIR   18
@@ -30,25 +37,24 @@
 #define PHI_MODE    32
 #define PHI_STEPS   200*8
 
-    
-
 int main(int argc,char**argv){
     trigger trigger;
     stepper rho,theta,phi;
     sphere sphere;
     int i;
-    verbose_l=L_ALL;
+    verbose_l=L_POSS;
 //    for(i=0;i<argc;i++)printf("%i: %s\n",i,argv[i]);
     if(!bcm2835_init())return 1;
     verbose(L_FALL,"α:");alert_led();
     trigger=trigger_init(MF,0,"μ");
+    rho=stepper_init(RHO_SLEEP,RHO_STEP,RHO_DIR,RHO_M0,RHO_M1,RHO_MODE,RHO_STEPS,"ρ");
     theta=stepper_init(THETA_SLEEP,THETA_STEP,THETA_DIR,THETA_M0,THETA_M1,THETA_MODE,THETA_STEPS,"θ");
     phi=stepper_init(PHI_SLEEP,PHI_STEP,PHI_DIR,PHI_M0,PHI_M1,PHI_MODE,PHI_STEPS,"φ");  
     sphere=sphere_init(rho,theta,phi,"ο");
     go(&sphere,0,90,270);
-    //trigger_shot(trigger,500000);
+    trigger_shot(trigger,500000);
     go(&sphere,0,180,180);
-    //trigger_shot(trigger,500000);
+    trigger_shot(trigger,500000);
     go(&sphere,0,0,0);
     stepper_off(phi);
     stepper_off(theta);
--- src/sphere.c
+++ src/sphere.c
@@ -20,18 +20,18 @@ sphere sphere_init(stepper rho,stepper theta,stepper phi,char *name){
 int go(sphere *sphere,double rho,double theta,double phi){
     char *msj;
     int n_rho,n_theta,n_phi,d_rho,d_theta,d_phi;
-//    int md_rho=((sphere->st_rho.mode*sphere->st_rho.steps)/2);
+    int md_rho=((sphere->st_rho.mode*sphere->st_rho.steps)/2);
     int md_theta=((sphere->st_theta.mode*sphere->st_theta.steps)/2);
     int md_phi=((sphere->st_phi.mode*sphere->st_phi.steps)/2);
-//    n_rho=rho*(STEPS_LOOP/360.0);    
+    n_rho=rho*(STEPS_LOOP/360.0);    
     n_theta=theta*(STEPS_LOOP/360.0);
     n_phi=phi*(STEPS_LOOP/360.0);
-//    d_rho=(((-1*md_rho)<=(d_rho=n_rho-sphere->s_rho))&&((md_rho)>=(d_rho=n_rho-sphere->s_rho)))?d_rho:((d_rho<0?1:-1)*((md_rho*2)-d_rho));
+    d_rho=(((-1*md_rho)<=(d_rho=n_rho-sphere->s_rho))&&((md_rho)>=(d_rho=n_rho-sphere->s_rho)))?d_rho:((d_rho<0?1:-1)*((md_rho*2)-d_rho));
     d_theta=(((-1*md_theta)<=(d_theta=n_theta-sphere->s_theta))&&((md_theta)>=(d_theta=n_theta-sphere->s_theta)))?d_theta:((d_theta<0?1:-1)*((md_theta*2)-d_theta));
     d_phi=(((-1*md_phi)<=(d_phi=n_phi-sphere->s_phi))&&((md_phi)>=(d_phi=n_phi-sphere->s_phi)))?d_phi:((d_phi<0?1:-1)*((md_phi*2)-d_phi));
-    asprintf(&msj,"%s: (i,%i,%i)<->(i,%i,%i).",sphere->name,sphere->s_theta,sphere->s_phi,n_theta,n_phi);
+    asprintf(&msj,"%s: (%i,%i,%i)<>(%i,%i,%i).",sphere->name,sphere->s_rho,sphere->s_theta,sphere->s_phi,n_rho,n_theta,n_phi);
     verbose(L_POSS,msj);free(msj);
-//    sphere->rho=rho;sphere->s_rho=n_rho;
+    sphere->rho=rho;sphere->s_rho=n_rho;
     sphere->theta=theta;sphere->s_theta=n_theta;
     sphere->phi=phi;sphere->s_phi=n_phi;
     stepper_walk_sync(sphere->st_theta,d_theta,sphere->st_phi,d_phi);
