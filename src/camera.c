d9e95c60c097cc35c8c4b43ca53f1ee147a69c5f
william.cordero <william.cordero@gmail.com>
2016-09-21 09:22:46 -0600

    add virtual camera

--- src/camera.c
+++ src/camera.c
@@ -30,6 +30,7 @@ int camera_off(camera *camera){
 double r2d(double r){return r*(180/M_PI);}
 double d2r(double d){return d*(M_PI/180);}
 int test(){
+    int x;
     double f=35;
     double v=15.6; //a77ii=23.5x15.6 35mm=36x24
     double h=23.5;
@@ -37,7 +38,20 @@ int test(){
     double aov_v=r2d(2*atan(v/(2*f)));
     double aov_h=r2d(2*atan(h/(2*f)));
     double aov_d=r2d(2*atan(d/(2*f)));
-    double a_v=360/(1+floor(360/aov_v));
+    double a_v=45;//360/(1+floor(360/aov_v));
     double a_h=360/(1+floor(360/aov_h));
+    double va=135;
     printf("\n  f=%fmm\n  v=%fmm\n  d=%fmm\naov_v=%f°\naov_h=%f°\naov_d=%f°\n  a_v=%f°\n  a_h=%f°\n\n",f,v,d,aov_v,aov_h,aov_d,a_v,a_h);
+    for(x=floor(va/a_v);0<x;x--){
+        double shift=((va-(aov_v*floor(va/aov_v)))/2)-(va/2);
+        double pos_phi=(x*aov_v)+((va-(aov_v*floor(va/aov_v))-va)/2);
+        printf(" x=%i>  phi=%f shift=%f\n",x,pos_phi,shift);
+    }
+    /*
+     * 
+     * (x*a)+((va-(a*floor(va/a))-va)/2)
+     * 
+     * (x*a)+((a*floor(va/a))/2)
+     * 
+     */
 }
