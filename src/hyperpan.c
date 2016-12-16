10c0ca4346c83cf71bf9a819136e515ceaa8c4ce
william.cordero <william.cordero@gmail.com>
2016-12-16 07:31:57 -0600

    Fixing virtual shot and adding sphere navegation

--- src/hyperpan.c
+++ src/hyperpan.c
@@ -1,21 +1,7 @@
 /*
  * William Cordero (2016) <william.cordero@gmail.com>
  */
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <argp.h>
-#ifndef  BCM_DUMMY
-#  include <bcm2835.h>
-#else  //BCM_DUMMY
-#  include "dummy/bcm_dummy.h"
-#endif //BCM_DUMMY
-#include "config.h"
 #include "verbose.h"
-#include "trigger.h"
-#include "stepper.h"
-#include "sphere.h"
 #include "camera.h"
 int main(int argc,char**argv){
     verbose_init(argc,argv);
